67572cac8064989b41ad0ad076f0ef5361b9737d
hwalborn <hwalborn@gmail.com>
2017-03-22 09:31:08 -0400

    that's so fetch omg

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const app = "I don't do much.";
+
+const toke = "625d0b6e3a5c134d1791a445a9da8b28b92691d8"
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json))
